641572d936675afd11bcac319a6f47bea6cc0999
Oca Yordan <oca.yordant.k@gmail.com>
2025-07-31 23:36:29 +0700

    init hello world

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import {View, Text} from 'react-native'
+
+const App = () => {
+    return (
+        <View>
+            <Text style={{textAlign: 'center', fontSize: 18}}>Hello World</Text>
+            <Text>Deskripsi 2</Text>
+        </View>
+    );
+}
+
+export default App;
